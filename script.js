72b938c87c178489bd3da438980b3d6d806a389f
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2024-09-18 08:44:18 -0400

    added content to sidebar

---
+++ script.js
@@ -0,0 +1,29 @@
+//sidebar
+
+window.onload = startUp();
+
+function startUp() {}
+
+function sidebar(param) {
+    let sidebar = document.getElementById("sidebar");
+    let cover = document.getElementById("darken");
+  if (param == 1) {
+    sidebar.classList.add("sidebar-open");
+    cover.classList.add("covering");
+    setTimeout(() => {
+        sidebar.classList.remove("sidebar-open");
+        sidebar.setAttribute("style", "left: 0;");
+        cover.classList.remove("covering");
+        cover.setAttribute("style", "opacity: 1; display: block;");
+    }, 500);
+  } else {
+    sidebar.classList.add("sidebar-close");
+    cover.classList.add("leaving");
+    setTimeout(() => {
+        sidebar.classList.remove("sidebar-close");
+        sidebar.setAttribute("style", "left: calc(var(--bar-width) * -1);");
+        cover.classList.remove("leaving");
+        cover.setAttribute("style", "opacity: 0;");
+    }, 500);
+  }
+}
